68b27e2ec0ea4e8d8b1521f5884446f319d5b1eb
RaghavKumar67 <107361476+RaghavKumar67@users.noreply.github.com>
2026-04-20 22:20:06 +0530

    Initial setup with Vite and React Router

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import JobDetails from './JobDetails';
+
+
+
+
+
+function App() {
+
+    return(
+
+    <BrowserRouter>
+    
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+    </nav>
+
+    <Routes>
+
+<Route path="/" element={<Home/>}/>
+<Route path="/dashboard" element={<Dashboard/>}/>
+<Route path="/jobs/:id" element={<JobDetails/>}/>
+    </Routes>
+    
+
+    </BrowserRouter>
+
+
+
+);
+ 
+}
+
+export default App;
---
+++ src/JobDetails.jsx
@@ -0,0 +1,5 @@
+function JobDetails() {
+  return <h1>JobDetails</h1>;
+}
+
+export default JobDetails;
